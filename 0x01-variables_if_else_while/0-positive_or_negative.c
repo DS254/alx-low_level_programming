10e5d91c0c0bded635dbf7fe80fc32bd2f759f0e
DS254 <dennissimiyu254@gmail.com>
2023-02-20 17:19:20 -0800

    0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,6 +10,7 @@
  */
 
 int main(void)
+
 {
 	int n;
 
@@ -21,6 +22,6 @@ int main(void)
 	else if (n == 0)
 		printf("%d is zero\n", n);
 	else
-		print("%d is negative\n", n);
+		printf("%d is positive\n", n);
 	return (0);
 }
