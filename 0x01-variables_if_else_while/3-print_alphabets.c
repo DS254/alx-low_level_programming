f6663bc8c1317b1a990312fec25d889e5f476eb0
DS254 <dennissimiyu254@gmail.com>
2023-02-20 18:09:53 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char c;
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'Z') {
+			putchar(c);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
